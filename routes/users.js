7d2ac4685c981841d3a54eb62f6420450bb928a1
abunasyr <a.syrmanov@okauto.kz>
2021-09-29 18:47:52 +0600

    feat: add errors

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
-  getUsers, getUserId, createUser, userInfo, avatarUpdate, login,
+  getUsers, getUserId, createUser, userInfo, avatarUpdate,
 } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:userId', getUserId);
+router.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24),
+  }),
+}), getUserId);
 router.post('/users', createUser);
-router.patch('/users/me', userInfo);
-router.patch('/users/me/avatar', avatarUpdate);
-router.post('/signin', login);
-router.post('/signup', createUser);
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(3).max(15),
+    about: Joi.string().required().min(3).max(15),
+  }),
+}), userInfo);
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string()
+      .regex(/^(https?:\/\/)?([\da-z\\.-]+)\.([a-z\\.]{2,6})([/\w \\.-]*)*\/?$/),
+  }),
+}), avatarUpdate);
 
 module.exports = router;// экспортировали роутер
